6f76f2d65b6b305b5672511b013c4adfb3c0ebad
metalboroda <89025071+metalboroda@users.noreply.github.com>
2021-09-01 10:35:18 +0300

    Initial commit

---
+++ app/src/main/java/com/mytodolist/mytodolist/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.mytodolist.mytodolist;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
